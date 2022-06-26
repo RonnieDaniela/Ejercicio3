0378dd46f402530940d0827169f3cfb7439cdef3
Daniela <fer.dany.et12@gmail.com>
2022-06-26 17:53:26 -0300

    Ejercicio 3

---
+++ main.js
@@ -0,0 +1,11 @@
+console.log("Hola Camping")
+
+let Nombre = prompt("Ingrese Su nombre")
+
+let Edad = prompt("Ingrese su edad")
+if (Edad < 30) {
+
+    console.log("Hola, Como andas?")
+} else {
+    console.log("Bienvenido. Buenos Dias")
+}
